e557b81095dd8f7ee2d0d0dbdfd19d2982b97834
sabrinabitix <118142475+sabrinabitix@users.noreply.github.com>
2022-11-17 07:40:44 +0700

    Add files via upload

---
+++ First-Submission-by-Sabrina/Sabrina_PU/main.py
@@ -0,0 +1,4 @@
+print("Nama: Sabrina Bitix Sania Putri")
+print("Jurusan: Information System")
+print("Universitas: President University")
+print("Alasan ingin masuk GDSC: Ingin menambah skill saya dibilang computer science")
